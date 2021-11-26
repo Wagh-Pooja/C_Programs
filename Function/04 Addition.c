a9d5ce881707308d0dc9cf3c27814c2ae2cd1fef
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2021-11-26 21:08:37 +0530

    Add files via upload

---
+++ Function/04 Addition.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+void Add(int,int);
+
+int main()
+{
+	int No1=0,No2=0;
+	
+	printf("\n Enter 2 Numbers for Addition=");
+	scanf("%d%d",&No1,&No2);
+	
+	Add(No1,No2);
+	
+	getch();
+	return 0;
+}
+
+void Add(int N1,int N2)
+{
+	int Sum=0;
+	
+	Sum=N1+N2;
+	
+	printf("\n Addition is = %d",Sum);
+	
+	return;
+}
