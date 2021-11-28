9f506c40eb45c522b8580e100a4cce8daa244e75
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2021-11-28 22:27:34 +0530

    Add files via upload

---
+++ Function/12 Using Functional Approach Maximum From Two Numbers.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Flag=0;
+
+int Find_Max(int,int);
+
+int main()
+{
+	int Num1=0,Num2=0,Max=0;
+	
+	printf("\n Enter Two Integer Numbers =");
+	scanf("%d%d",&Num1,&Num2);
+	
+	Max=Find_Max(Num1,Num2);
+	
+	(Flag==1) ? printf("\n Both Given Numbers Are Equal..") : printf("\n Maximum Number From Given Numbers is = %d",Max);
+	
+	printf("\n Thanks !!!");
+	
+	getch();
+	return 0;
+}
+
+int Find_Max(int N1,int N2)
+{
+	if(N1==N2)
+	{
+		Flag=1;
+		return -1;
+	}
+	if(N1>N2)
+	{
+		return N1;
+	}
+	return N2;
+}
