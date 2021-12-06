fc723b78797b0f211ef202e4e5279dff9d763554
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2021-12-06 22:03:39 +0530

    Add files via upload

---
+++ Pointer/12 Pointer Arithmetic- Decrement.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int Num[5]={15,25,35,45,55};
+	int *ptr=&Num[3];
+	printf("\n Base Address Of Array Is=%d",Num);
+	printf("\n Value Of Pointer Is=%d",ptr);
+	printf("\n Value Of Array Element Where Pointer Pointing=%d",*ptr);
+	
+	getch();
+	
+	ptr--;
+	
+	printf("\n\n New Value In Pointer Is =%d",ptr);
+	printf("\n\n Value Of Array Element Where Pointer Pointing =%d",*ptr);
+	
+	getch();
+	return 0;
+}
