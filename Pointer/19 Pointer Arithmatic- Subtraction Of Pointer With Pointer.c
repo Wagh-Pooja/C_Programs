3f9c516f8d2d320456436f7a4347e70de90cf709
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2021-12-09 21:47:19 +0530

    Add files via upload

---
+++ Pointer/19 Pointer Arithmatic- Subtraction Of Pointer With Pointer.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int No1=21, No2=50;
+	int *Res=NULL;
+	int *ptr1=&No1;
+	int *ptr2=&No2;
+	
+	printf("\n Base Address Of Variable 1st Is=%d",&No1);
+	printf("\n Base Address Of Variable 2nd  Is=%d",&No2);
+	
+	printf("\n Value Of Pointer1 Is=%d",ptr1);
+	printf("\n Value Of Pointer2 Is=%d",ptr2);
+	
+	printf("\n Value Of Variable To Which  Pointer Pointing=%d",*ptr1);
+	printf("\n Value Of Variable To Which  Pointer Pointing=%d",*ptr2);
+	
+	getch();
+	
+	Res= ptr2 - ptr1;
+	
+	printf("\n\n Value Result Is =%d",Res);
+	
+	getch();
+	return 0;
+}
