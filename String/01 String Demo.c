b86c8c20adbeb770cffca310ac4b301bd8867c49
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2021-12-10 21:21:27 +0530

    Add files via upload

---
+++ String/01 String Demo.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char cArr[20]={'k','a','r','a','d'};
+	puts(cArr);
+	puts("Enter a String=");
+	gets(cArr);
+	
+	puts("Given String is =");
+	puts(cArr);
+	
+	getch();
+	return 0;
+}
