e9f75a7229e246362dd6a7af29954da71d214eb6
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2021-12-11 21:25:52 +0530

    Add files via upload

---
+++ String/04 String Length In Main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char cSrc[50]={'\0'};
+	int Len=0;
+	
+	puts(" Enter a String=");
+	gets(cSrc);
+	
+	for(Len=0; cSrc[Len] != '\0'; Len++);
+	
+	printf("\n Length Of Given String Is = %d",Len);
+	
+	getch();
+	return 0;
+}
