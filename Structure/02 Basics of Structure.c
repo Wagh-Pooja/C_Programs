9a4dfa8e9733a47913cb49649afd7038533a271a
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2021-12-22 23:17:59 +0530

    Add files via upload

---
+++ Structure/02 Basics of Structure.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct student
+{
+	int RollNo;
+	char Name[80];
+	char City[20];
+	float Per;
+};
+
+int main()
+{
+	struct student std1,std2;
+	
+	std1.RollNo=65;
+	strcpy(std1.Name,"Amey Wagh");
+	strcpy(std1.City,"Pune");
+	std1.Per=75.95647;
+	
+	printf("\n Enter Roll No =");
+	scanf("%d",&std2.RollNo);
+	
+	printf("\n Enter Name =");
+	scanf("%s",&std2.Name);
+	
+	printf("\n Enter City =");
+	scanf("%s",&std2.City);
+	
+	printf("\n Enter Percentage =");
+	scanf("%f",&std2.Per);
+	
+	printf("\n 1st Student Details Given are = \n\n\t RollNo =%d \n\t Name = %s \n\t City = %s \n\t Percentage = %f\n",std1.RollNo,std1.Name,std1.City,std1.Per);
+	
+	printf("\n 2nd Student Details Given are = \n\n\t RollNo = %d \n\t Name = %s \n\t City = %s \n\t Percentage = %f\n",std2.RollNo,std2.Name,std2.City,std2.Per);
+	
+	getch();
+	return 0;
+}
