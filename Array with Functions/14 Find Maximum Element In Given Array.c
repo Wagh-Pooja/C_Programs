e409e0ca000293e98c1a3bebae495de3c07867dc
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-01-14 19:11:30 +0530

    Add files via upload

---
+++ Array with Functions/14 Find Maximum Element In Given Array.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#define size 5
+
+void Accept_Element(int[]);
+void Display_Element(int[]);
+int Find_Max_Element(int[]);
+
+int main()
+{
+    int Num[size]={},Max=0;
+    
+    Accept_Element(Num);
+    Display_Element(Num);
+    
+    getch();
+    
+    Max=Find_Max_Element(Num);
+    
+    printf("\nMaximum Elements in Given Array is %d",Max);
+    getch();
+    return 0;
+}
+
+int Find_Max_Element(int Arr[])
+{
+    int i=0,Top=0;
+    for(i=0;i<size;i++)
+    {
+        if(i==0)
+        {
+            Top=Arr[i];
+            continue;
+        }
+        if(Arr[i]>Top)
+        {
+            Top=Arr[i];
+        }
+    }
+    return Top;
+}
+
+void Accept_Element(int Arr[])
+{
+    int i=0;
+    for(i=0;i<size;i++)
+    {
+        printf("\nEnter Value for Element Number %d=>",i+101);
+        scanf("%d",&Arr[i]);
+    }
+    return;
+}
+
+void Display_Element(int fun[])
+{
+    int i=0;
+    for(i=0;i<size;i++)
+    {
+        printf("\nValue of Element %d=%d",i+1,fun[i]);
+    }
+    return;
+}
