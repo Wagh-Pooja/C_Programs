b78f7a7c1f5bc54bf9611d94b85d33eb33fbbaf5
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-02-03 20:55:39 +0530

    Add files via upload

---
+++ String with Functions/01 String Length Using Function.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int strLenX(char *);
+
+int main()
+{
+	char cSrc[20]={'\0'};
+	int Len=0;
+	
+	printf("\n Enter a String=");
+	gets(cSrc);
+	
+	Len= strLenX(cSrc);
+	
+	printf("\n Length Of Given String Is =%d",Len);
+	
+	getch();
+	return 0;
+}
+
+int strLenX(char *cPtr)
+{
+	int Cnt=0;
+	
+	while(cPtr[Cnt] != '\0')
+	{
+		Cnt++;
+	}
+	return Cnt;
+}
