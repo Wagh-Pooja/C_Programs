ce97e38a817a096789406b9e5ad79af71e2f7ae0
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-04-07 15:03:37 +0530

    Marvellous Infosystems C Programs

---
+++ Storage Classes/04_extern_Storage_Class.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+//extern int no;
+int no;        //global variable
+
+void fun()
+{
+    int i = 11;
+    int j;
+}
+
+int main()
+{
+    printf("\n Inside main");
+    fun();
+    return 0;
+}
