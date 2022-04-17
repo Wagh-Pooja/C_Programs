558e8506e38064d476b1149d2bf267d65e19de1e
Wagh Pooja Maruti <91519272+Wagh-Pooja@users.noreply.github.com>
2022-04-17 16:44:24 +0530

    Marvellous Infosystems C Programs

---
+++ Array Programs/02_Member_By_Member_Initialization_Technique.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int Arr[5];
+	
+	Arr[0] = 10;
+	Arr[1] = 20;
+	Arr[2] = 30;
+	Arr[3] = 40;
+	Arr[4] = 50;
+	
+	printf("\n %d",Arr[4]);
+	
+	printf("\n %d",Arr[2]);
+}
